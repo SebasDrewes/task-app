05e2b4329b8697a7ba9a7df292a4d8caebeb8518
Sebastian Drewes <sebas.drewes@gmail.com>
2021-07-24 09:43:11 -0300

    added id to divs

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Overview({tasks , deleteTask, editTask}) {
 
     const listOfTasks = tasks.map((task, index) =>
-    <div>
+    <div key={task.id}>
      <ul key={task.id}>({index + 1}){" "}{task.text}</ul>
      <button onClick={() => editTask(task.id, "hello")}>Editar Task</button>
      <button onClick={() => deleteTask(task.id)}>Eliminar Task</button>
